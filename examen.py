3a2ab5f80ce60b40feaf3eee9d8e0b128fcec17d
NetAcad DEVASC <changeme@example.com>
2024-06-18 21:29:17 +0000

    examen.py

---
+++ examen.py
@@ -0,0 +1,10 @@
+# Solicitar datos al usuario
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Por favor, ingresa tu apellido: ")
+edad = input("Por favor, ingresa tu edad: ")
+
+# Imprimir los datos ingresados
+print("\nDatos ingresados:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Edad:", edad)
